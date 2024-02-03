20fbf6453234ebd07db952b99246ac360d26a85c
unknown <abubakarsarwar198@gmail.com>
2024-02-03 23:30:27 +0500

    style fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,12 @@
 const AboutPage = () => {
   return (
     <div className="container">
-      <div className="about">
-        <span className="about-word">A FEW WORDS ABOUT ME</span>
-        <h1 className="about-title">
+      <div className="about-page">
+        <span className="about-page-word">A FEW WORDS ABOUT ME</span>
+        <h1 className="about-page-title">
           I’m Damian Watracz, a multidisciplinary designer who focuses on telling my clients’ stories visually, through enjoyable and meaningful experiences. I specialize in responsive websites and functional user interfaces.
         </h1>
-        <p className="about-para">
+        <p className="about-page-para">
           Over the past 9 years I have been working with big companies and rising startups around the world as a designer and art director, working solo and leading small design teams. In my spare time I enjoy photography and motorcycle adventures.
         </p>
       </div>
