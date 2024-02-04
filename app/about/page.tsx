27ce05ee603d2298e29bf2e4f2817b4f362cfbf6
unknown <abubakarsarwar198@gmail.com>
2024-02-05 01:05:15 +0500

    about content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,10 @@ const AboutPage = () => {
       <div className="about-page">
         <span className="about-page-word">A FEW WORDS ABOUT ME</span>
         <h1 className="about-page-title">
-          I’m Damian Watracz, a multidisciplinary designer who focuses on telling my clients’ stories visually, through enjoyable and meaningful experiences. I specialize in responsive websites and functional user interfaces.
+          I’m Muhammad Abu Bakar, I'm an eager, goal-oriented programmer with a passion for coding
+          and a solid grounding in a variety of web development tools. My experience includes designing and
+          implementing user interfaces, optimizing website performance, and ensuring seamless functionality across
+          different browsers.
         </h1>
         <p className="about-page-para">
           Over the past 9 years I have been working with big companies and rising startups around the world as a designer and art director, working solo and leading small design teams. In my spare time I enjoy photography and motorcycle adventures.
