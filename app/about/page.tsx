27f4f5b9e5c743c5167e29c7c99663ba1e96d3d8
unknown <abubakarsarwar198@gmail.com>
2024-02-06 23:34:44 +0500

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,9 +5,7 @@ const AboutPage = () => {
         <span className="about-page-word">A FEW WORDS ABOUT ME</span>
         <h1 className="about-page-title">
           I’m Muhammad Abu Bakar, I'm an eager, goal-oriented programmer with a passion for coding
-          and a solid grounding in a variety of web development tools. My experience includes designing and
-          implementing user interfaces, optimizing website performance, and ensuring seamless functionality across
-          different browsers.
+          and a solid grounding in a variety of web development tools.
         </h1>
         <p className="about-page-para">
           Over the past 9 years I have been working with big companies and rising startups around the world as a designer and art director, working solo and leading small design teams. In my spare time I enjoy photography and motorcycle adventures.
